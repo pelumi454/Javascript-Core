0054107ee13ce4352729d5eec763129eb286218d
pelumi454 <amoochristianah454@gmail.com>
2022-08-09 13:30:14 +0100

    add examples

--- 01-data-modelling-with-JS/dataModelling.js
+++ 01-data-modelling-with-JS/dataModelling.js
@@ -1,1 +1,21 @@
 alert("Thy kingdom come to the aid of the JavaScript");
+
+//  String Template Literals
+let fhru = "FHRU";
+const ageBracket = `${fhru}`;
+console.log(ageBracket);
+
+let items = cucumbers;
+let pricePerCucumber = 0.5;
+let quantity = 5;
+`You bought ${quantity} {items} for $${pricePerCucumber * quantity}`;
+
+//Math Object
+Math.floor(3.033333333333333); //3
+//How to generate numbers between 1 and 10
+Math.random() * 10;
+let randomNumbers = Math.floor(Math.random() * 10 + 1);
+console.log(randomNumbers);
+
+// PARSE INT (to turn a string to a number)
+parseInt("45")
