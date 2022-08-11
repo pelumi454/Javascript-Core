5a05cd2edc664652c1eebd2b19bb57b52bb19209
pelumi454 <amoochristianah454@gmail.com>
2022-08-11 11:08:13 +0100

    learn about callback functions in javascript

---
+++ 07-advance-look-at-functions/advance-functions.js
@@ -0,0 +1,34 @@
+// write a function to double the items of an array
+function doubleItem(arr) {
+    let result = [];
+    for (let num of arr) {
+       let double = num * 2;
+        result.push(double);
+    }
+}
+// console.log(doubleItem([1, 2, 3, 4, 5]));
+
+//passing function as arguments
+function rage() {
+     console.log('I am angry');
+}
+ 
+function repeatNthTime(action, num) {
+    for (let i = 0; i < num; i++){
+        action();
+    }
+}
+repeatNthTime(rage, 5);
+
+//write a function to check numbers between 0 and 18
+function makeBetweenFunc(x, y) {
+    return function(num) {
+        num <=x && num >= y;
+    }
+}
+makeBetweenFunc(0, 18)
+const isChild = makeBetweenFunc(0, 18);
+console.log(isChild(18));
+
+//
+
