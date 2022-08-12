a1afb6a80d17d8cc5671df1ef38b82402ad6c1ea
pelumi454 <amoochristianah454@gmail.com>
2022-08-12 09:55:11 +0100

    computed properties in javascript

---
+++ 010-object-and-this-keyword/object-and-this-keyword.js
@@ -0,0 +1,19 @@
+// computed properties
+const role = 'admin';
+const person = 'John';
+
+const team = {
+    role: person,
+}
+// role here is equal to John
+// team = {
+//     role: 'John'
+// }
+// to get the value of the role key we need to
+team[role] = person;
+// {admin = 'John'} or
+// {
+//     [role]: person
+// }
+
+// methods are functions that are attached to objects
