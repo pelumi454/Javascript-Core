0e23b80a75132450fa672a74cf4393eabcd4a6d3
pelumi454 <amoochristianah454@gmail.com>
2022-08-12 10:48:57 +0100

    build annoymatic demo

--- 010-object-and-this-keyword/object-and-this-keyword.js
+++ 010-object-and-this-keyword/object-and-this-keyword.js
@@ -23,3 +23,30 @@ const math = {
     }
 }
 // math.add(1,2)
+
+// the THIS keyword in methods
+const peter = {
+    first: 'John',
+    last: 'Bellion',
+    nickName: 'Jelly',
+    fullName: function () {
+        const {first, last} = this;
+        return `${first} ${last}`;
+    }
+}
+
+// annoymatic demo
+const annoyer = {
+    phrases: ['literally', 'cray cray', 'i can haz cheezburger', 'goodbye!', 'can leave', 'i am a robot'],
+    pickPhrase: function () { 
+        const {phrases} = this;
+        const idx = Math.floor(Math.random() * phrases.length);
+        return phrases[idx];
+    },
+    start: function () {
+        setInterval(() => {
+            console.log(this.pickPhrase());
+        }, 1000);
+    }
+}
+// annoyer.pickPhrase();
