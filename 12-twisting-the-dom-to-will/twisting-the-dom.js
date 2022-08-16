cc2eda63b854617991bbddb44f17c632d7be7374
pelumi454 <amoochristianah454@gmail.com>
2022-08-16 09:27:09 +0100

    fix: refactor code for nba scores example

--- 12-twisting-the-dom-to-will/twisting-the-dom.js
+++ 12-twisting-the-dom-to-will/twisting-the-dom.js
@@ -85,25 +85,13 @@ const warriorsGames = [
   },
 ];
 
-const ulParent = document.createElement('ul');
-for (let game of warriorsGames) {
-  const { homeTeam, awayTeam } = game;
-  const gameLi = document.createElement("li");
-  const { team: hTeam, points: hPoints } = homeTeam;
-  const { team: aTeam, points: aPoints } = awayTeam;
-  const teamNames = `${aTeam} @ ${hTeam}`;
-  let scoreLine;
-  if (aPoints > hPoints) {
-    scoreLine = `<b>${aPoints}</b>-${hPoints}`;
-  } else {
-    scoreLine = `${aPoints}-<b>${hPoints}</b>`;
+const makeChart = (games, targetTeam) => {
+  const ulParent = document.createElement("ul");
+  for (let game of games) {
+    const gameLi = document.createElement("li");
+    gameLi.innerHTML = getScoreLine(game);
+    gameLi.classList.add(isWinner(game, targetTeam) ? "win" : "loss");
+    ulParent.appendChild(gameLi);
   }
-  const warriors = hTeam === "Golden State" ? homeTeam : awayTeam;
-  gameLi.classList.add(warriors.isWinner ? "win" : "loss");
-
-  gameLi.innerHTML = `${teamNames} ${scoreLine}`;
-  console.log(scoreLine);
-  ulParent.appendChild(gameLi);
-}
-
-document.body.prepend(ulParent);
+  return ulParent;
+};
