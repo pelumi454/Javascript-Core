31f978de4d31b8e63b47f43b005cc7bc6bed3449
pelumi454 <amoochristianah454@gmail.com>
2022-08-16 10:21:12 +0100

    fix: events on multiple elements

--- 13-eventListeners/eventListeners.js
+++ 13-eventListeners/eventListeners.js
@@ -1,9 +1,15 @@
-const colors = [
-    'red',
-    'orange',
-    'yellow',
-    'green',
-    'blue',
-    'indigo',
-    'violet'
-];
+const colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+
+const changeColor = function () {
+  const h1 = document.querySelector("h1");
+  h1.style.color = this.style.backgroundColor;
+};
+
+const container = document.querySelector("#boxes");
+for (let color of colors) {
+  const box = document.createElement("div");
+  box.classList.add("box");
+  box.style.backgroundColor = color;
+  container.appendChild(box);
+  box.addEventListener("click", changeColor);
+}
