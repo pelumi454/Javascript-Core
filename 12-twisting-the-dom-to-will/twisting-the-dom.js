733fa6a747259bb66f6ca3a7be4211275d1b7a76
pelumi454 <amoochristianah454@gmail.com>
2022-08-16 09:29:32 +0100

    fix: append element

--- 12-twisting-the-dom-to-will/twisting-the-dom.js
+++ 12-twisting-the-dom-to-will/twisting-the-dom.js
@@ -111,3 +111,12 @@ const getScoreLine = ({ homeTeam, awayTeam }) => {
   }
   return `${teamNames} ${scoreLine}`;
 };
+const gsSection = document.querySelector("#gs");
+const houstonSection = document.querySelector("#hr");
+
+const gsChart = makeChart(warriorsGames, "Golden State");
+const hrChart = makeChart(warriorsGames, "Houston");
+
+//Append them!
+gsSection.appendChild(gsChart);
+houstonSection.appendChild(hrChart);
