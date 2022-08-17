76a21ab79f82bf3246b0e8edb90de600a4047d05
pelumi454 <amoochristianah454@gmail.com>
2022-08-17 11:32:18 +0100

    fix: create a fetch example

--- 15-making-HTTP-requests/making-HTTP-request.js
+++ 15-making-HTTP-requests/making-HTTP-request.js
@@ -1,1 +1,8 @@
 // using fetch
+fetch("https://swapi.co/api/planets/")
+  .then((response) => response.json())
+  .then((data) => {
+    for (let planet of data.results) {
+      console.log(planet.name);
+    }
+  });
