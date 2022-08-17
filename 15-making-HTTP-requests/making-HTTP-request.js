c99b976a73f9e9b30c3cb55d8eabb864a67c883b
pelumi454 <amoochristianah454@gmail.com>
2022-08-17 11:46:39 +0100

    fix: chaining fetch requests

--- 15-making-HTTP-requests/making-HTTP-request.js
+++ 15-making-HTTP-requests/making-HTTP-request.js
@@ -25,3 +25,28 @@ fetch("https://swapi.co/api/planets/")
         console.log("SOMETHING WENT WRONG WITH FETCH!");
         console.log(err);
       });
+//  or to chain the fetch requests
+fetch("https://swapi.co/api/planets/")
+  .then((response) => {
+    if (!response.ok) throw new Error(`Status Code Error: ${response.status}`);
+
+    return response.json();
+  })
+  .then((data) => {
+    console.log("FETCHED ALL PLANETS (first 10)");
+    const filmURL = data.results[0].films[0];
+    return fetch(filmURL);
+  })
+  .then((response) => {
+    if (!response.ok) throw new Error(`Status Code Error: ${response.status}`);
+
+    return response.json();
+  })
+  .then((data) => {
+    console.log("FETCHED FIRST FILM, based off of first planet");
+    console.log(data.title);
+  })
+  .catch((err) => {
+    console.log("SOMETHING WENT WRONG WITH FETCH!");
+    console.log(err);
+  });
