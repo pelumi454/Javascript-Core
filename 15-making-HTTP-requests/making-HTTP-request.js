ec91d85448fe976c5e1c4b03e3716df9ac93626d
pelumi454 <amoochristianah454@gmail.com>
2022-08-17 11:40:50 +0100

    fix: catch error in fetch

--- 15-making-HTTP-requests/making-HTTP-request.js
+++ 15-making-HTTP-requests/making-HTTP-request.js
@@ -1,8 +1,27 @@
 // using fetch
 fetch("https://swapi.co/api/planets/")
-  .then((response) => response.json())
-  .then((data) => {
-    for (let planet of data.results) {
-      console.log(planet.name);
-    }
-  });
+    .then((response) => response.json())
+    .then((data) => {
+        for (let planet of data.results) {
+            console.log(planet.name);
+        }
+    }).catch((error) => { 
+        console.log('something went wrong');
+        console.log(error);
+    }); 
+    // or in order to catch the error because fetch returns a response whether it is the response you want or not
+    fetch("https://swapi.co/api/planetsuy21/")
+      .then((response) => {
+        if (!response.ok)
+          throw new Error(`Status Code Error: ${response.status}`);
+
+        response.json().then((data) => {
+          for (let planet of data.results) {
+            console.log(planet.name);
+          }
+        });
+      })
+      .catch((err) => {
+        console.log("SOMETHING WENT WRONG WITH FETCH!");
+        console.log(err);
+      });
